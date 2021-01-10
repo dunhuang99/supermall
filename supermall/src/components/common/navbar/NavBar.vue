1213595a82962dd348f994db12c2a3e3ea29b3b2
qidunhang <qidunhang@baijiahulian.com>
2021-01-11 00:46:31 +0800

    supermall-home

---
+++ supermall/src/components/common/navbar/NavBar.vue
@@ -0,0 +1,37 @@
+<!--  -->
+<template>
+<div class='nav-bar'>
+    <div class="left">
+        <slot name="left"></slot>
+    </div>
+    <div class="center">
+        <slot name="center"></slot>
+    </div>
+    <div class="right">
+        <slot name="right"></slot>
+    </div>
+</div>
+</template>
+
+<script>
+
+export default {
+}
+</script>
+<style >
+    .nav-bar {
+        display: flex;
+        height: 44px;
+        line-height: 44px;
+        /* background-color: blue; */
+        text-align: center;
+    }
+    .left,.right {
+        width: 60px;
+        /* background-color: red; */
+    }
+    .center {
+        flex: 1;
+        /* background-color: yellow; */
+    }
+</style>
---
+++ supermall/src/views/home/Home.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="home">
+    <nav-bar class="home-nav">
+      <div slot="center">
+        购物街
+      </div>
+    </nav-bar>
+    <!-- <home-swiper :banners="banners"></home-swiper> -->
+  </div>
+</template>
+<script>
+    import NavBar from 'components/common/navbar/NavBar.vue'
+    // import HomeSwiper from './childComps/HomeSwiper'
+    import { getHomeMultidata } from 'network/home.js'
+
+    export default {
+      name: 'Home',
+       data() {
+        return {
+          banners: [],
+          recommends: []
+        }
+      },
+      components: {
+        NavBar,
+        // HomeSwiper,
+      },
+       created() {
+        getHomeMultidata().then(res => {
+          this.banners = res.data.banner.list
+          this.recommends = res.data.recommend.list
+          console.log(this.banners);
+        })
+      },
+    }
+</script>
+<style >
+  .home-nav {
+    background-color: var(--color-tint);
+    color: #fff;
+  }
+</style>
